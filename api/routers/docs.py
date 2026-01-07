00855f5768910ff1bce1f5b8dde32d42983b45bc
Diêgo Tavares Canafistula <diego.canafistula@gmail.com>
2026-01-07 14:23:15 -0300

    proteção de endpoints com autenticacao

--- api/routers/docs.py
+++ api/routers/docs.py
@@ -1,12 +1,18 @@
+from http import HTTPStatus
 from io import BytesIO
+from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import StreamingResponse
+from sqlalchemy.orm import Session
 
+from api.database import get_session
+from api.schemas.models import User
 from api.schemas.projetos.formularioenelce import ProjetoFormularioEnelCe
 from api.schemas.projetos.memorial import ProjetoMemorial
 from api.schemas.projetos.procuracao import ProjetoProcuracao
 from api.schemas.projetos.unifilar import ProjetoUnifilar
+from api.security import get_current_user
 from src.buildingdocuments.formularioENEL import FormularioEnelCe
 from src.buildingdocuments.memorialdescritivo import MemorialDescritivo
 from src.buildingdocuments.procuracao import Procuracao
@@ -16,14 +22,23 @@ from src.domain.creatediagramobject import ObjetoDiagramaUnifilar
 from src.domain.creatememorialobject import ObjetosCalculados
 
 router = APIRouter(prefix="/docs", tags=["Documentos"])
+Sessions = Annotated[Session, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
 # FAZER UM SCHEMA SÓ PRO MEMORIAL.
 @router.post("/memorialdescritivo", status_code=201, response_model=None)
-async def post_data_memorial(dados_entrada: ProjetoMemorial):
-    projeto = ProjectFactory.factory(dados_entrada)
+async def post_data_memorial(
+    dados_entrada: ProjetoMemorial,
+    current_user: CurrentUser,
+    user_id: int,
+):
+    if current_user.id != user_id:
+        raise HTTPException(
+            status_code=HTTPStatus.FORBIDDEN, detail="Not enough permissions"
+        )
 
-    print(projeto)
+    projeto = ProjectFactory.factory(dados_entrada)
     retorno = ObjetosCalculados(
         projeto
     )  # O OBJETO DE RETORNO É UM OBJ DATACLASS
@@ -43,7 +58,16 @@ async def post_data_memorial(dados_entrada: ProjetoMemorial):
 
 
 @router.post("/procuracao", status_code=201, response_model=None)
-async def post_data_procuracao(projeto: ProjetoProcuracao):
+async def post_data_procuracao(
+    projeto: ProjetoProcuracao,
+    current_user: CurrentUser,
+    user_id: int,
+):
+    if current_user.id != user_id:
+        raise HTTPException(
+            status_code=HTTPStatus.FORBIDDEN, detail="Not enough permissions"
+        )
+
     pdf = Procuracao(projeto)
     pdf.gerar_procuracao()
 
@@ -57,7 +81,16 @@ async def post_data_procuracao(projeto: ProjetoProcuracao):
 
 
 @router.post("/diagramaunifilar", status_code=201, response_model=None)
-async def post_data_diagrama_unifilar(dados: ProjetoUnifilar):
+async def post_data_diagrama_unifilar(
+    dados: ProjetoUnifilar,
+    current_user: CurrentUser,
+    user_id: int,
+):
+    if current_user.id != user_id:
+        raise HTTPException(
+            status_code=HTTPStatus.FORBIDDEN, detail="Not enough permissions"
+        )
+
     projeto = ObjetoDiagramaUnifilar(dados).construir_dados_diagrama()
     pdf = DiagramaUnifilar(projeto)
     pdf.gerar_diagrama()
@@ -73,7 +106,17 @@ async def post_data_diagrama_unifilar(dados: ProjetoUnifilar):
 
 
 @router.post("/formularioenel", status_code=201, response_model=None)
-async def post_data_formulario_enel_ce(dados: ProjetoFormularioEnelCe):
+async def post_data_formulario_enel_ce(
+    dados: ProjetoFormularioEnelCe,
+    current_user: CurrentUser,
+    user_id: int,
+):
+
+    if current_user.id != user_id:
+        raise HTTPException(
+            status_code=HTTPStatus.FORBIDDEN, detail="Not enough permissions"
+        )
+
     pdf = FormularioEnelCe(dados)
     pdf.gerar_formulario()
 
